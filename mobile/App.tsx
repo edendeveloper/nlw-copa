78e79529b2fa84c840b54b7228edb0dad33b29ad
Welington Manfrin <welington.robertodev@gmail.com>
2022-11-15 16:46:20 -0300

    Navegação no app mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,10 +3,8 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 import { Loading } from './src/components/Loading';
-import { Pools } from "./src/screens/Pools";
-
 import { THEME } from './src/styles/theme';
-import { SignIn } from "./src/screens/SignIn";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
@@ -20,7 +18,7 @@ export default function App() {
           translucent
         />
 
-        { fontsLoaded ? <SignIn /> : <Loading /> }
+        { fontsLoaded ? <Routes /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
